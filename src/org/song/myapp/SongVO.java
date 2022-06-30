f776eaf5aecd2849060aa79233ec2105ef5d8868
Lina <092874@naver.com>
2022-07-01 02:10:08 +0900

    add controller

--- src/org/song/myapp/SongVO.java
+++ src/org/song/myapp/SongVO.java
@@ -1,4 +1,62 @@
 package org.song.myapp;
 
+import java.util.Objects;
+
 public class SongVO {
+    private String title;
+    private String singer;
+    private String genre;
+
+    public SongVo(String title, String singer, String genre){
+        this.title = title;
+        this.singer = singer;
+        this.genre = genre;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SongVO that = (SongVO) o;
+        return Objects.equals(title, that.title) && Objects.equals(singer, that.singer) && Objects.equals(genre, that.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, singer, genre);
+    }
+
+    @Override
+    public String toString() {
+        return "SongController{" +
+                "title='" + title + '\'' +
+                ", singer='" + singer + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
+
+
 }
